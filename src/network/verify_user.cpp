0f92ae2543dd43cf8dd9459bdce878b0c3efb1a9
zhupengfei <zhupf321@gmail.com>
2018-10-27 15:35:01 +0800

    network, web_service: Add Verification backend and use new lobby API
    Added verify_backend to load user_data for members. and removed method to generate UID as this is now done server-side.
    
    Added GetUsername function and a "token" param to room_member.
    Also added a username to ChatEntry, so that the username can be shown (along with nicknames) in the chat dialog.

---
+++ src/network/verify_user.cpp
@@ -0,0 +1,20 @@
+// Copyright 2018 Citra Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include "network/verify_user.h"
+
+namespace Network::VerifyUser {
+
+Backend::~Backend() = default;
+
+NullBackend::~NullBackend() = default;
+
+UserData NullBackend::LoadUserData([[maybe_unused]] const std::string& verify_UID,
+                                   [[maybe_unused]] const std::string& token) {
+    return {};
+}
+
+} // namespace Network::VerifyUser
